7ad15a9012d3d7c402313dba921eee2847341e24
KMichta <kacperm@soconnect.co.uk>
2022-05-27 15:36:30 +0100

    WindowsFeatureUpdate

---
+++ Get-WindowsFeatureUpdate.ps1
@@ -0,0 +1,12 @@
+if (!(Get-PackageProvider -ListAvailable -Name Nuget -ErrorAction SilentlyContinue)){
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$false
+}
+
+if (!(Get-Module -ListAvailable -Name PSWindowsUpdate -ErrorAction SilentlyContinue)) {
+    Install-Module -Name PSWindowsUpdate -Force -Confirm:$false
+}
+else {
+    Import-Module -Name PSWindowsUpdate
+}
+
+Install-WindowsUpdate -Title "Feature Update" -AcceptAll -Install -IgnoreReboot -IgnoreRebootRequired| Write-Output
