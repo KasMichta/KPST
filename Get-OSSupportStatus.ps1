d4699487fb62b03d0144e0a83bd54c5e196a43bf
KMichta <kacperm@soconnect.co.uk>
2022-05-27 14:31:41 +0100

    Error Handling

--- Get-OSSupportStatus.ps1
+++ Get-OSSupportStatus.ps1
@@ -6,23 +6,28 @@ Function Get-OSSupportStatus {
     $Edition = @{l = 'Edition'; e = { if ($_.OsOperatingSystemSKU -match 'Ent|iot') { 'E' }else { 'W' } } }
     $OS = Get-ComputerInfo | Select-object -Property $Version, $Edition, OSName
 
-    Write-Output "OS Detected: $($OS.OSName)"
-    Write-Output "OS Version: $($OS.Version)"
-
-    $MatchingOS = $OSEOLs | Where-Object {
-    ($_.cycleshorthand -eq $OS.Version) -and 
-    ($($_.cycle | Select-String -Pattern '\((?<ed>E|W)\)' -AllMatches | Set-Variable Editions ; $Editions.Matches.Groups.where({ $_.name -eq 'ed' }).value) -contains $OS.Edition) }
-
-    $eoldate = Get-Date $MatchingOS.eol
-    $OSSupported = (Get-Date) -lt $eoldate
-
-    If ($OSSupported) {
-        Write-Output 'OS Supported'
+    if (($null -eq $OS.Version) -or ($null -eq $OS.OSName)) {
+        Write-Output "System failed to retrieve OS version or OS name. Likely needs updates or reboot."
     }
     else {
-        Write-Output 'OS is EOL'
-    }
 
+        Write-Output "OS Detected: $($OS.OSName)"
+        Write-Output "OS Version: $($OS.Version)"
+
+        $MatchingOS = $OSEOLs | Where-Object {
+            ($_.cycleshorthand -eq $OS.Version) -and 
+            ($($_.cycle | Select-String -Pattern '\((?<ed>E|W)\)' -AllMatches | Set-Variable Editions ; $Editions.Matches.Groups.where({ $_.name -eq 'ed' }).value) -contains $OS.Edition) }
+
+        $eoldate = Get-Date $MatchingOS.eol
+        $OSSupported = (Get-Date) -lt $eoldate
+
+        If ($OSSupported) {
+            Write-Output 'OS Supported'
+        }
+        else {
+            Write-Output 'OS is EOL'
+        }
+    }
 }
 
 Get-OSSupportStatus
