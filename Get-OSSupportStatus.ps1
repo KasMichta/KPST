2c57d4131590d57989bd1ea638ce1dd788341490
KasMichta <kacper.michta33@gmail.com>
2023-04-26 13:42:35 +0100

    Fix matching

--- Get-OSSupportStatus.ps1
+++ Get-OSSupportStatus.ps1
@@ -3,23 +3,26 @@ Function Get-OSSupportStatus {
     $OSEOLs = Invoke-RestMethod -Method Get -Uri https://endoflife.date/api/windows.json
 
     $Version = @{l = 'Version'; e = { $_.OSVersion } }
-    $Edition = @{l = 'Edition'; e = { if ($_.OsOperatingSystemSKU -match 'Ent|iot') { 'E' }else { 'W' } } }
-    $OS = Get-ComputerInfo | Select-object -Property $Version, $Edition, OSName
+    $Edition = @{l = 'Edition'; e = { if ($_.OsOperatingSystemSKU -match 'Ent|iot|Edu') { 'E' }else { 'W' } } }
+    $OS = Get-ComputerInfo | Select-Object -Property $Version, $Edition, OSName
 
     if (($null -eq $OS.Version) -or ($null -eq $OS.OSName)) {
-        Write-Output "System failed to retrieve OS version or OS name. Likely needs updates or reboot."
+        Write-Output 'System failed to retrieve OS version or OS name. Likely needs updates or reboot.'
     }
     else {
 
         Write-Output "OS Detected: $($OS.OSName)"
         Write-Output "OS Version: $($OS.Version)"
 
+        $pattern = '\({0}\)' -f $os.Edition
         $MatchingOS = $OSEOLs | Where-Object {
-            ($_.cycleshorthand -eq $OS.Version) -and 
-            ($($_.cycle | Select-String -Pattern '\((?<ed>E|W)\)' -AllMatches | Set-Variable Editions ; $Editions.Matches.Groups.where({ $_.name -eq 'ed' }).value) -contains $OS.Edition) }
+            ($_.latest -eq $OS.Version) -and 
+            ($_.cycle -match $pattern) 
+        }
 
         $eoldate = Get-Date $MatchingOS.eol
-        $OSSupported = (Get-Date) -lt $eoldate
+        # Trigger if EOL is within 2 months
+        $OSSupported = (Get-Date) -lt $eoldate.AddMonths(-2)
 
         If ($OSSupported) {
             Write-Output 'OS Supported'
