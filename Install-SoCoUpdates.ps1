fd56dfbdf8991a5be8ab61c529774bfaf5c0fb21
KasMichta <33022848+KasMichta@users.noreply.github.com>
2023-07-13 15:49:36 +0100

    Extra logging and whitespace fix

--- Install-SoCoUpdates.ps1
+++ Install-SoCoUpdates.ps1
@@ -2,8 +2,8 @@
 
 # Function to check if any user is logged in
 function IsUserLoggedIn {
-    $loggedInUser = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty UserName
-    return (![string]::IsNullOrEmpty($loggedInUser))
+	$loggedInUser = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty UserName
+	return (![string]::IsNullOrEmpty($loggedInUser))
 }
 
 # Check if a user is logged in
@@ -14,15 +14,15 @@ $userLoggedIn = IsUserLoggedIn
 # Search and download updates
 $UpdateSession = New-Object -ComObject Microsoft.Update.Session
 $UpdateSearcher = $UpdateSession.CreateUpdateSearcher()
-$SearchResult = $UpdateSearcher.Search("IsInstalled=0")
+$SearchResult = $UpdateSearcher.Search('IsInstalled=0')
 
 
 $Updates = New-Object System.Collections.ArrayList
 
 # Filter for Feature Updates
-$SearchResult.Updates | Where-Object { $_.Title -match "Feature update to Windows" } | % {[void]$Updates.add($_)}
+$SearchResult.Updates | Where-Object { $_.Title -match 'Feature update to Windows' } | ForEach-Object { [void]$Updates.add($_) }
 # Filter for Critical, Security, Rollup, and Definition updates
-$SearchResult.Updates | Where-Object {$_.Categories | Where-Object {$_.Name -match "Critical|Security|Update rollup|Definition" }} | % {[void]$Updates.add($_)}
+$SearchResult.Updates | Where-Object { $_.Categories | Where-Object { $_.Name -match 'Critical|Security|Update rollup|Definition' } } | ForEach-Object { [void]$Updates.add($_) }
 
 # If there are Updates available, proceed with the installation process
 if ($Updates) {
@@ -47,14 +47,18 @@ if ($Updates) {
 
 	# Reboot the device if the installation was successful and no user is logged in
 	if ($InstallResult.ResultCode -eq 2 -and !(IsUserLoggedIn)) {
-                Write-Output "Updates Installed, User Not Logged in - Rebooting..."
+		Write-Output '----Install Successful----'
+		Write-Output 'Updates Installed, User Not Logged in - Rebooting...'
 		Restart-Computer
-	} elseif ($InstallResult.ResultCode -eq 2 -and (IsUserLoggedIn)) {
-		Write-Output "----Install Successful----"
-		Write-Output "User Logged in, Reboot cancelled."
-	}else{
-		Write-Output "!!!-Install Unsuccessful-!!!"
 	}
-} else {
-	Write-Output "No relevant Updates are available."
+ elseif ($InstallResult.ResultCode -eq 2 -and (IsUserLoggedIn)) {
+		Write-Output '----Install Successful----'
+		Write-Output 'User Logged in, Reboot cancelled.'
+	}
+	else {
+		Write-Output '!!!-Install Unsuccessful-!!!'
+	}
+}
+else {
+	Write-Output 'No relevant Updates are available.'
 }
